a4c2c2738250a9bed1dc94a15e09e490b9806a43
hritu raj <rajhritu06@gmail.com>
2024-06-16 01:06:30 +0530

    part 5 : filter category

--- src/features/product-list/ProductApi.js
+++ src/features/product-list/ProductApi.js
@@ -1,9 +1,29 @@
 // A mock function to mimic making an async request for data
-export function fetchCount(amount = 1) {
+export function fetchAllProducts() {
   return new Promise(async (resolve) => {
-    const response = await fetch("http://localhost:8080");
+    // TODO
 
-    const data = await response.data();
+    const response = await fetch("http://localhost:8080/products");
+
+    const data = await response.json();
+
+    resolve({ data });
+  });
+}
+
+export function fetchProductsByFilters(filter) {
+  let queryString = "";
+  for (let key in filter) {
+    queryString += `${key}=${filter[key]}&`;
+  }
+
+  return new Promise(async (resolve) => {
+    const response = await fetch(
+      "http://localhost:8080/products?" + queryString
+    );
+
+    const data = await response.json();
+
     resolve({ data });
   });
 }
--- src/features/product-list/components/ProductList.js
+++ src/features/product-list/components/ProductList.js
@@ -1,11 +1,15 @@
-import React, { useState, Fragment } from "react";
+import React, { useState, Fragment, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/20/solid";
+import {
+  ChevronLeftIcon,
+  ChevronRightIcon,
+  StarIcon,
+} from "@heroicons/react/20/solid";
 import {
   decrement,
-  increment,
-  incrementAsync,
-  selectCount,
+  fetchAllProductsAsync,
+  selectAllProducts,
+  fetchProductsByFiltersAsync,
 } from "../ProductSlice";
 
 import { Dialog, Disclosure, Menu, Transition } from "@headlessui/react";
@@ -18,49 +22,42 @@ import {
   Squares2X2Icon,
 } from "@heroicons/react/20/solid";
 import { Link } from "react-router-dom";
+const fallbackImage = "path/to/fallback/image.jpg";
 
 const sortOptions = [
-  { name: "Most Popular", href: "#", current: true },
-  { name: "Best Rating", href: "#", current: false },
-  { name: "Newest", href: "#", current: false },
-  { name: "Price: Low to High", href: "#", current: false },
-  { name: "Price: High to Low", href: "#", current: false },
+  { name: "Best Rating", sort: "rating", order: "desc", current: false },
+  { name: "Price: Low to High", sort: "price", order: "asc", current: false },
+  { name: "Price: High to Low", sort: "price", order: "desc", current: false },
 ];
 
 const filters = [
   {
-    id: "color",
-    name: "Color",
-    options: [
-      { value: "white", label: "White", checked: false },
-      { value: "beige", label: "Beige", checked: false },
-      { value: "blue", label: "Blue", checked: true },
-      { value: "brown", label: "Brown", checked: false },
-      { value: "green", label: "Green", checked: false },
-      { value: "purple", label: "Purple", checked: false },
-    ],
-  },
-  {
     id: "category",
     name: "Category",
     options: [
-      { value: "new-arrivals", label: "New Arrivals", checked: false },
-      { value: "sale", label: "Sale", checked: false },
-      { value: "travel", label: "Travel", checked: true },
-      { value: "organization", label: "Organization", checked: false },
-      { value: "accessories", label: "Accessories", checked: false },
+      { value: "smartphones", label: "smartphones", checked: false },
+      { value: "electronics", label: "electronics", checked: false },
+      { value: "clothing", label: "clothing", checked: false },
+      { value: "accessories", label: "accessories", checked: false },
+      { value: "food", label: "food", checked: false },
+      { value: "baby", label: "baby", checked: false },
+      { value: "fragrances", label: "fragrances", checked: false },
+      { value: "skincare", label: "skincare", checked: false },
+      { value: "groceries", label: "groceries", checked: false },
+      { value: "home-decoration", label: "home decoration", checked: false },
     ],
   },
   {
-    id: "size",
-    name: "Size",
+    id: "brand",
+    name: "Brands",
     options: [
-      { value: "2l", label: "2L", checked: false },
-      { value: "6l", label: "6L", checked: false },
-      { value: "12l", label: "12L", checked: false },
-      { value: "18l", label: "18L", checked: false },
-      { value: "20l", label: "20L", checked: false },
-      { value: "40l", label: "40L", checked: true },
+      { value: "fauji", label: "fauji", checked: false },
+      { value: "Dry Rose", label: "Dry Rose", checked: false },
+      { value: "Boho Decor", label: "Boho Decor", checked: false },
+      { value: "Flying Wooden", label: "Flying Wooden", checked: false },
+      { value: "LED Lights", label: "LED Lights", checked: false },
+      { value: "luxury palace", label: "luxury palace", checked: false },
+      { value: "Golden", label: "Golden", checked: false },
     ],
   },
 ];
@@ -69,54 +66,31 @@ function classNames(...classes) {
   return classes.filter(Boolean).join(" ");
 }
 
-const products = [
-  {
-    id: 1,
-    name: "Basic Tee",
-    href: "#",
-    imageSrc:
-      "https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg",
-    imageAlt: "Front of men's Basic Tee in black.",
-    price: "$35",
-    color: "Black",
-  },
-  {
-    id: 2,
-    name: "Basic Tee",
-    href: "#",
-    imageSrc:
-      "https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg",
-    imageAlt: "Front of men's Basic Tee in black.",
-    price: "$35",
-    color: "Black",
-  },
-  {
-    id: 3,
-    name: "Basic Tee",
-    href: "#",
-    imageSrc:
-      "https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg",
-    imageAlt: "Front of men's Basic Tee in black.",
-    price: "$35",
-    color: "Black",
-  },
-  {
-    id: 4,
-    name: "Basic Tee",
-    href: "#",
-    imageSrc:
-      "https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg",
-    imageAlt: "Front of men's Basic Tee in black.",
-    price: "$35",
-    color: "Black",
-  },
-  // More products...
-];
-
 export default function ProductList() {
-  const count = useSelector(selectCount);
   const dispatch = useDispatch();
   const [mobileFiltersOpen, setMobileFiltersOpen] = useState(false);
+  const products = useSelector(selectAllProducts) || [];
+  const [filter, setFilter] = useState({});
+
+  useEffect(() => {
+    dispatch(fetchAllProductsAsync(filter));
+  }, [dispatch]);
+
+  const handleChange = (e, section, option) => {
+    const newFilter = { ...filter, [section.id]: option.value };
+    setFilter(newFilter);
+
+    dispatch(fetchProductsByFiltersAsync(newFilter));
+    console.log(section.id, option.value);
+  };
+
+  const handleSort = (e, option) => {
+    const newFilter = { ...filter, _sort: option.sort, _order: option.order };
+    setFilter(newFilter);
+
+    dispatch(fetchProductsByFiltersAsync(newFilter));
+    console.log(option.sort, option.order);
+  };
 
   return (
     <div>
@@ -210,6 +184,9 @@ export default function ProductList() {
                                             defaultValue={option.value}
                                             type="checkbox"
                                             defaultChecked={option.checked}
+                                            onChange={(e) =>
+                                              console.log(e.target.value)
+                                            }
                                             className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
                                           />
                                           <label
@@ -266,8 +243,8 @@ export default function ProductList() {
                           {sortOptions.map((option) => (
                             <Menu.Item key={option.name}>
                               {({ active }) => (
-                                <a
-                                  href={option.href}
+                                <p
+                                  onClick={(e) => handleSort(e, option)}
                                   className={classNames(
                                     option.current
                                       ? "font-medium text-gray-900"
@@ -277,7 +254,7 @@ export default function ProductList() {
                                   )}
                                 >
                                   {option.name}
-                                </a>
+                                </p>
                               )}
                             </Menu.Item>
                           ))}
@@ -356,6 +333,9 @@ export default function ProductList() {
                                       defaultValue={option.value}
                                       type="checkbox"
                                       defaultChecked={option.checked}
+                                      onChange={(e) =>
+                                        handleChange(e, section, option)
+                                      }
                                       className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
                                     />
                                     <label
@@ -380,38 +360,55 @@ export default function ProductList() {
                     <div className="bg-white">
                       <div className="mx-auto max-w-2xl px-4 py-0 sm:px-6 sm:py-0 lg:max-w-7xl lg:px-8">
                         <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:gap-x-8">
-                          {products.map((product) => (
-                            <Link to={"/product-details"}>
-                              <div key={product.id} className="group relative">
-                                <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
-                                  <img
-                                    src={product.imageSrc}
-                                    alt={product.imageAlt}
-                                    className="h-full w-full object-cover object-center lg:h-full lg:w-full"
-                                  />
-                                </div>
-                                <div className="mt-4 flex justify-between">
-                                  <div>
-                                    <h3 className="text-sm text-gray-700">
-                                      <a href={product.href}>
-                                        <span
-                                          aria-hidden="true"
-                                          className="absolute inset-0"
-                                        />
-                                        {product.name}
-                                      </a>
-                                    </h3>
-                                    <p className="mt-1 text-sm text-gray-500">
-                                      {product.color}
-                                    </p>
+                          {products &&
+                            products.map((product) => (
+                              <Link to={"/product-details"}>
+                                <div
+                                  key={product.id}
+                                  className="group relative border-solid border-2 border-gray-200 p-2"
+                                >
+                                  <div className=" min-h-60  aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-60">
+                                    <img
+                                      src={product.thumbnail}
+                                      alt={product.title}
+                                      className="h-full w-full object-cover object-center lg:h-full lg:w-full"
+                                    />
                                   </div>
-                                  <p className="text-sm font-medium text-gray-900">
-                                    {product.price}
-                                  </p>
+                                  <div className="mt-4 flex justify-between">
+                                    <div>
+                                      <h3 className="text-sm text-gray-700">
+                                        <div href={product.thumbnail}>
+                                          <span
+                                            aria-hidden="true"
+                                            className="absolute inset-0"
+                                          />
+                                          {product.title}
+                                        </div>
+                                      </h3>
+                                      <p className="mt-1 text-sm text-gray-500">
+                                        <StarIcon className="w-6 h-6 inline "></StarIcon>
+                                        <span className="align-bottom">
+                                          {product.rating}
+                                        </span>
+                                      </p>
+                                    </div>
+                                    <div>
+                                      <p className="text-sm block font-medium text-gray-900">
+                                        💲
+                                        {Math.round(
+                                          product.price *
+                                            (1 -
+                                              product.discountPercentage / 100)
+                                        )}
+                                      </p>
+                                      <p className="text-sm block line-through font-medium text-gray-400">
+                                        💲{product.price}
+                                      </p>
+                                    </div>
+                                  </div>
                                 </div>
-                              </div>
-                            </Link>
-                          ))}
+                              </Link>
+                            ))}
                         </div>
                       </div>
                     </div>
